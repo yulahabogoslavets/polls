dd524f2b3df46dc85327e99f9f23d240b67daee3
yulahabogoslavets <test@test.email>
2025-04-09 11:29:52 +0200

    feat(wip): save data in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import { Poll } from './components/Poll';
 
-const dataArray = [
-  {
-    name: 'Page A',
-    value: 1,
-  },
-  {
-    name: 'Page B',
-    value: 1,
-  },
-  {
-    name: 'Page C',
-    value: 1,
-  },
-];
-
 function App() {
+  const [dataArray, setDataArray] = useState([]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('dataArray'));
+    setDataArray(storedData);
+  }, []);
+
   return (
     <>
       <main className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
         <h1 className="text-3xl font-bold">My Poll</h1>
 
         <div className="flex flex-wrap gap-4 items-center justify-center my-8">
-          <Poll dataArray={dataArray} color="#05def3" />
+          {dataArray.length
+            ? dataArray.map((item, index) => {
+                return <Poll key={index} dataArray={item} />;
+              })
+            : 'No Polls Available, create one!'}
         </div>
       </main>
     </>
--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -1,47 +1,56 @@
 import { Bar, BarChart, XAxis } from 'recharts';
 import { useState } from 'react';
 
-interface PollProps {
-  dataArray: Array<{ name: string; value: number }>;
-  color: string;
+interface Option {
+  name: string;
+  value: number;
 }
 
-export function Poll({ dataArray, color }: PollProps) {
+interface DataItem {
+  title: string;
+  color: string;
+  options: Option[];
+}
+interface PollProps {
+  dataArray: DataItem[];
+}
+
+export function Poll({ dataArray }: PollProps) {
   const [data, setData] = useState(dataArray);
   const [pollStatus, setPollStatus] = useState(0);
 
   const onPollChange = (event: React.MouseEvent<HTMLButtonElement>) => {
     const objName = (event.target as HTMLButtonElement).innerText;
-    const newData = data.filter((item) => {
+    const newData = data.options.filter((item) => {
       if (item.name !== objName) {
         return item;
       }
       return { ...item, value: item.value++ };
     });
 
-    setData(newData);
+    setData({ ...data, options: newData });
     setPollStatus(1);
   };
   return (
     <>
       <div className="bg-gray-400 p-8 border-gray-800 rounded-lg width-full h-[400px] flex flex-col items-center justify-center gap-4">
-        <h2 className="mb-4">What Movie do you want to see?</h2>
+        <h2 className="mb-4">{data.title}</h2>
         {
           {
             1: (
               <BarChart
-                data={data}
+                data={data.options}
                 margin={{ bottom: 60 }}
                 width={300}
                 height={300}
               >
-                <Bar dataKey="value" fill={color} />
+                <Bar dataKey="value" fill={data.color} />
                 <XAxis dataKey={'name'} interval={0} angle={-10} dy={32} />
               </BarChart>
             ),
             0: (
               <div className="flex items-center justify-center gap-4 flex-wrap">
-                {data.map((item, index) => {
+                {data.options.map((item, index) => {
                   return (
                     <button
                       key={index}
