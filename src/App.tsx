67e4669d361ceb7adef1f95bc2217fd5422b36f4
yulahabogoslavets <test@test.email>
2025-04-09 09:29:53 +0200

    feat: install recharts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import './App.css';
+import { BarChart, Bar, ResponsiveContainer, XAxis } from 'recharts';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+];
 
 function App() {
   return (
     <>
       <h1 className="text-red-500">hello</h1>
+      <ResponsiveContainer width="100%" height={500}>
+        <BarChart data={data}>
+          <Bar dataKey="uv" fill="#8884d8" />
+          <XAxis dataKey={'name'} />
+        </BarChart>
+      </ResponsiveContainer>
     </>
   );
 }
