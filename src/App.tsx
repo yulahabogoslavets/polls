b7dd5042d542feeb562538d04b7f40caeee22759
yulahabogoslavets <test@test.email>
2025-04-09 09:49:19 +0200

    feat: add buttons for vote

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,53 @@
 import './App.css';
 import { BarChart, Bar, ResponsiveContainer, XAxis } from 'recharts';
+import { useState } from 'react';
 
-const data = [
+const dataArray = [
   {
     name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    value: 1,
   },
   {
     name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    value: 1,
   },
   {
     name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    value: 1,
   },
 ];
 
 function App() {
+  const [data, setData] = useState(dataArray);
+
+  const onPollChange = (event) => {
+    const objName = event?.target.innerText;
+    const newData = data.filter((item) => {
+      if (item.name !== objName) {
+        return item;
+      }
+      return { ...item, value: item.value++ };
+    });
+
+    setData(newData);
+  };
   return (
     <>
       <h1 className="text-red-500">hello</h1>
       <ResponsiveContainer width="100%" height={500}>
-        <BarChart data={data}>
-          <Bar dataKey="uv" fill="#8884d8" />
-          <XAxis dataKey={'name'} />
+        <BarChart data={data} margin={{ bottom: 120 }}>
+          <Bar dataKey="value" fill="#8884d8" />
+          <XAxis dataKey={'name'} interval={0} angle={-10} dy={32} />
         </BarChart>
       </ResponsiveContainer>
+
+      {data.map((item, index) => {
+        return (
+          <button key={index} onClick={onPollChange}>
+            {item.name}
+          </button>
+        );
+      })}
     </>
   );
 }
