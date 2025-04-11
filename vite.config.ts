e809ee6bd70d95f86ef5255a5397f08bc3b1edd8
yulahabogoslavets <test@test.email>
2025-04-11 11:42:11 +0200

    fix(ci): chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     base: '/polls',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom'],
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000,
+    },
 })
